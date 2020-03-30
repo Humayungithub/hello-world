d42a76537d9289a242c34dfbb0dbce19b797b3c2
Humayungithub <humayun2911@gmail.com>
2020-03-30 19:06:30 +0600

    Adding HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>	//Pre-processor directive(Header file for input output functions)
+int main() //main function declaration
+{
+   printf("Hello World\n");	//to output the string on a display
+   return 0;	//terminating function
+}
